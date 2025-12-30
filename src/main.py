b9229b60e648527995f81e8759f33b3b4044c979
Tomas McCandless <tomas.mccandless@gmail.com>
2025-12-30 00:38:06 -0600

    connect to scale

--- src/main.py
+++ src/main.py
@@ -5,14 +5,12 @@ from typing import Tuple
 from scale import Scale
 from valve import Valve
 
-app = FastAPI()
-
-
 def initialize_hardware() -> Tuple[Scale, Valve]:
     if COLDBREW_IS_PROD:
         from LunarScale import LunarScale
         from MotorKitValve import MotorKitValve
         s: Scale = LunarScale(COLDBREW_SCALE_MAC_ADDRESS)
+        s.connect()
         v: Valve = MotorKitValve()
     else:
         from scale import MockScale
@@ -21,7 +19,9 @@ def initialize_hardware() -> Tuple[Scale, Valve]:
         v: Valve= MockValve()
     return s, v
 
-my_scale, my_valve = initialize_hardware()
+scale, valve = initialize_hardware()
+
+app = FastAPI()
 
 @app.get("/")
 def read_root():
@@ -29,25 +29,25 @@ def read_root():
 
 @app.get("/scale")
 def read_weight():
-    weight = my_scale.get_weight()
-    battery_pct = my_scale.get_battery_percentage()
-    units = my_scale.get_units()
+    weight = scale.get_weight()
+    battery_pct = scale.get_battery_percentage()
+    units = scale.get_units()
     return {"weight": weight, "battery_pct": battery_pct, "units": units}  # Placeholder value in grams
 
 @app.post("/valve/step_forward")
 def step_forward():
-    my_valve.step_forward()
+    valve.step_forward()
     return {"status": "stepped forward"}  # Placeholder response
 
 @app.post("/valve/step_backward")
 def step_backward():
-    my_valve.step_backward()
+    valve.step_backward()
     return {"status": "stepped backward"}  # Placeholder response
 
 # TODO probably want to move breadcrumbs clientside and have an endpoint that steps a certain number of times
 @app.post("/valve/return_to_start")
 def return_to_start():
-    my_valve.return_to_start()
+    valve.return_to_start()
     return {"status": "returned to start"}  # Placeholder response
 
 
