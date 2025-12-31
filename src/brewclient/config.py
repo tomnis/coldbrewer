a3a029771b58c72939fb48a285a44e2c7ffb18df
Tomas McCandless <tomas.mccandless@gmail.com>
2025-12-30 22:19:18 -0600

    typo

--- src/brewclient/config.py
+++ src/brewclient/config.py
@@ -1,4 +1,4 @@
 import os
 
 # TODO probably want a better name for this
-COLDBREW_VALVE_URL = 'http://coldbrewer.local:8000' # os.environ.get('COLDBREW_VALVE_URL', 'http://localhost:8000')
+COLDBREW_VALVE_URL = os.environ.get('COLDBREW_VALVE_URL', 'http://localhost:8000')
