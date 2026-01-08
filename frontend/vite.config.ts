55646f89a5e2d93d0fd94939f73d8d57dd15f083
Tomas McCandless <tomas.mccandless@gmail.com>
2026-01-07 22:32:07 -0600

    allowed domains

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envPrefix: 'COLDBREW_FRONTEND_'
+  envPrefix: 'COLDBREW_FRONTEND_',
+  server: {
+      allowedHosts: [
+        'coldbrewer.local',
+        'pi4.local'
+      ]
+    },
 })
